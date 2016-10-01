684ba02cd72868044689ceca0ca4fed73d034727
Tomas Korcak <korczis@gmail.com>
2016-10-01 16:56:21 +0200

    Send welcome message

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,7 +2,7 @@ defmodule MicrocrawlerWebapp.RoomChannel do
   use Phoenix.Channel
 
   def join("room:lobby", _message, socket) do
-    {:ok, socket}
+    {:ok, %{msg: "Welcome!"}, socket}
   end
   def join("room:" <> _private_room_id, _params, _socket) do
     {:error, %{reason: "unauthorized"}}
