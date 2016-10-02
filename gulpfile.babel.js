cc0c1151658ec6bc258c0372526eaff15096b65e
Tomas Korcak <korczis@gmail.com>
2016-10-02 10:53:50 +0200

    Working sourcemaps

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,6 +5,7 @@ import mocha from 'gulp-mocha';
 import flow from 'gulp-flowtype';
 import gutil from 'gulp-util';
 import react from 'gulp-react';
+import sourcemaps from 'gulp-sourcemaps';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 
@@ -12,7 +13,8 @@ import webpackConfig from './webpack.config.babel';
 
 const path = {
     sources: [
-        'client/src/**/*.js'
+        'client/src/**/*.js',
+        'client/src/**/*.jsx'
     ],
     tests: [
         'client/test/**/*.js'
@@ -24,8 +26,14 @@ gulp.task('default', ['build']);
 
 gulp.task('babel', () => {
     return gulp.src(path.sources)
+        .pipe(sourcemaps.init())
         .pipe(babel())
-        .pipe(react({stripTypes: true}))
+        // .pipe(react({stripTypes: true}))
+        .pipe(sourcemaps.write('../../web/static/assets/js/maps', {
+          sourceMappingURL: (file) => {
+            return '/js/maps/' + file.relative + '.map';
+          }
+        }))
         .pipe(gulp.dest(path.lib));
 });
 
@@ -36,7 +44,7 @@ gulp.task('lint', () => {
     // So, it's best to have gulp ignore the directory as well.
     // Also, Be sure to return the stream from the task;
     // Otherwise, the task may end before the stream has finished.
-    return gulp.src([path.sources[0],'!node_modules/**'])
+    return gulp.src([...path.sources,'!node_modules/**'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
         .pipe(eslint())
@@ -75,8 +83,8 @@ gulp.task('watch-test', () => {
 gulp.task('webpack', ['test'], (callback) => {
     var myConfig = Object.create(webpackConfig);
     myConfig.plugins = [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
+        // new webpack.optimize.DedupePlugin(),
+        // new webpack.optimize.UglifyJsPlugin()
     ];
 
     // run webpack
