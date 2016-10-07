e8b3d5aab7304fdbd390ad0dde4c2ff145cb256f
Tomas Korcak <korczis@gmail.com>
2016-10-07 23:12:56 +0200

    Simple socket connection for React app

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -7,7 +7,23 @@ import Nav from 'react-bootstrap/lib/Nav';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import NavItem from 'react-bootstrap/lib/NavItem';
 
+import { Socket } from 'phoenix';
+
 export default class App extends PureComponent {
+  componentDidMount() {
+    const socket = new Socket('/socket', {
+      params: { token: window.userToken || null }
+    });
+    socket.connect();
+
+
+    const channel = socket.channel('worker:lobby', {});
+    channel.join()
+      .receive('error', () => {
+        console.log('Connection error');
+      });
+  }
+
   render() {
     const styles = require('./App.scss');
 
