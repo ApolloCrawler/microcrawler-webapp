cd9824cf1c5428e96fba21b90e199f319f8b87db
Lubos Vesely <lv.bossek@gmail.com>
2016-10-08 18:01:08 +0200

    Send worker updates to client channel

--- lib/microcrawler_webapp/active_workers.ex
+++ lib/microcrawler_webapp/active_workers.ex
@@ -4,6 +4,7 @@ defmodule MicrocrawlerWebapp.ActiveWorkers do
   require Logger
 
   alias MicrocrawlerWebapp.WorkerChannel
+  alias MicrocrawlerWebapp.ClientChannel
 
   def start_link() do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
@@ -13,11 +14,12 @@ defmodule MicrocrawlerWebapp.ActiveWorkers do
     GenServer.call(__MODULE__, :joined_workers)
   end
 
-  def update_joined_worker_info(user) do
-    GenServer.cast(__MODULE__, {:update_joined_worker_info, self, user})
+  def update_joined_worker_info(worker) do
+    GenServer.cast(__MODULE__, {:update_joined_worker_info, self, worker})
   end
 
   def init(state) do
+    ClientChannel.clear_worker_list()
     WorkerChannel.send_joined_workers_info()
     {:ok, state}
   end
@@ -26,15 +28,17 @@ defmodule MicrocrawlerWebapp.ActiveWorkers do
     {:reply, Map.values(state), state}
   end
 
-  def handle_cast({:update_joined_worker_info, pid, user}, state) do
+  def handle_cast({:update_joined_worker_info, pid, worker}, state) do
     unless Map.has_key?(state, pid) do
         Process.monitor(pid)
     end
-    {:noreply, Map.put(state, pid, user)}
+    ClientChannel.update_worker(worker)
+    {:noreply, Map.put(state, pid, worker)}
   end
 
   def handle_info({:DOWN, _ref, :process, pid, reason}, state) do
     Logger.debug inspect(reason)
+    ClientChannel.remove_worker(Map.fetch!(state, pid))
     {:noreply, Map.delete(state, pid)}
   end
 end
