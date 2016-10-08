c4a3811156bb243e3d674cf4adf168574cde2c0c
Lubos Vesely <lv.bossek@gmail.com>
2016-10-08 17:25:25 +0200

    Handle ActiveWorkers process crashes

--- lib/microcrawler_webapp/active_workers.ex
+++ lib/microcrawler_webapp/active_workers.ex
@@ -3,6 +3,8 @@ defmodule MicrocrawlerWebapp.ActiveWorkers do
 
   require Logger
 
+  alias MicrocrawlerWebapp.WorkerChannel
+
   def start_link() do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
@@ -11,21 +13,28 @@ defmodule MicrocrawlerWebapp.ActiveWorkers do
     GenServer.call(__MODULE__, :joined_workers)
   end
 
-  def worker_joined(user) do
-    GenServer.cast(__MODULE__, {:worker_joined, self, user})
+  def update_joined_worker_info(user) do
+    GenServer.cast(__MODULE__, {:update_joined_worker_info, self, user})
+  end
+
+  def init(state) do
+    WorkerChannel.send_joined_workers_info()
+    {:ok, state}
   end
 
   def handle_call(:joined_workers, _from, state) do
     {:reply, Map.values(state), state}
   end
 
-  def handle_cast({:worker_joined, pid, user}, state) do
-    ref = Process.monitor(pid)
-    {:noreply, Map.put(state, {ref, pid}, user)}
+  def handle_cast({:update_joined_worker_info, pid, user}, state) do
+    unless Map.has_key?(state, pid) do
+        Process.monitor(pid)
+    end
+    {:noreply, Map.put(state, pid, user)}
   end
 
-  def handle_info({:DOWN, ref, :process, pid, reason}, state) do
+  def handle_info({:DOWN, _ref, :process, pid, reason}, state) do
     Logger.debug inspect(reason)
-    {:noreply, Map.delete(state, {ref, pid})}
+    {:noreply, Map.delete(state, pid)}
   end
 end
